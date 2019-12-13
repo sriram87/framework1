4e271d974a380827be5254f70e915477f3022f17
Sriram <sriram.ramanujam1@gmail.com>
2019-12-13 19:57:53 +0530

    Changed

--- src/main/java/Package1/BasePage.java
+++ src/main/java/Package1/BasePage.java
@@ -44,7 +44,7 @@ public class BasePage {
 			}
 		}
 
-		public String getTxtAttribute(WebElement element) {
+		public String getTxtAttribute(WebElement element)1234 {
 			return element.getAttribute("value");
 		}
 
